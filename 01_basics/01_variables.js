53533a16f5b91f8a7dc5dd4b96fd1f305a092c90
danialkhan8284 <mdaniyalkhan8284@gmail.com>
2026-02-26 01:11:41 +0500

    variable file added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 12348;
+let accoutEmail = "danial@gmail.com";
+var accountPassword = "32f35";
+accountCity = "Rawalpindi";
+
+console.table([accountId, accoutEmail, accountPassword, accountCity]);
