4aa0c1a1278852fe37b8095053862dac790eadc1
danialkhan8284 <mdaniyalkhan8284@gmail.com>
2026-02-26 23:44:08 +0500

    add datatype file

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,17 @@
+"use strict";
+let name = "danial";
+let age = 21;
+let isloggedin = false;
+let state = null;
+let typee;
+
+// name => String
+// age => Number
+// isloggedin => Boolean
+// state => standable value or variable
+// typee => undefined
+// Symbol => unique
+
+console.log(typeof null );   //object
+console.log(typeof undefined); //undefined
+
