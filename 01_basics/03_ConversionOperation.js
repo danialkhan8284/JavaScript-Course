90d21389146d4e89477698d6cdd09413bc1db177
danialkhan8284 <mdaniyalkhan8284@gmail.com>
2026-03-01 00:47:34 +0500

    add Conversion and Operation file

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,31 @@
+// let age = "23";
+
+// console.log(typeof age);
+
+// let valinNumber = Number(age);
+
+// console.log(typeof valinNumber);
+// console.log(valinNumber);
+// console.log(valinNumber  + age);
+
+
+// ******* operation ********
+
+let str1 = "hello";
+let str2 = " Danial";
+let str3 = str1 + str2;
+
+console.log(str3);
+
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
+
+console.log(2 + 2);
+console.log(2 - 1);
+console.log(2 * 2);
+console.log (2**2);    //power
+console.log(2%2);   //reminder
