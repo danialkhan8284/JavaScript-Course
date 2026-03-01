e4bc4ba16040e29f3b8180f22807988f60a352fd
danialkhan8284 <mdaniyalkhan8284@gmail.com>
2026-03-02 01:07:51 +0500

    add datatype summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,34 @@
+// Premitive
+
+//  7 types: string, boolean, number, symbol, null, undefined, bigInt
+  
+  const score = 23;
+  const name = "danial";
+  const isLoggedin = false;
+  const tempmail = null;
+  const  useremail = undefined;
+
+  const id = Symbol('123');
+  const anotherId = Symbol('123');
+
+  console.log(id === anotherId);   //false output
+
+  const bigNumber = 34325235323155335633n;
+
+
+
+
+
+//  non premitive (Reference)
+
+// object, Array, functions
+
+let obj = {
+    name : "danial",
+    age : 23
+};
+const arr = ["hello", "world", "how"];
+
+const myfunction = function(){
+    console.log("hello danial");
+}
